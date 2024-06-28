c83bdf325b2d5de6d6b080f9ad3147e74bec967a
MgrGracz <kamil.bandzwolek@gmail.com>
2024-06-28 22:38:22 +0200

    v002

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages
+    ] });
 
 client.once('ready', () => {
     console.log('bot is ready');
     client.user.setActivity('!help', { type: 'WATCHING' });
 
+});
+
+
+client.on('messageCreate', async message => {
+
+    console.log(message.content);
+    if (message.content.toLowerCase() === 'czesc') {
+        message.channel.send('Cześć!');
+    }
 });
 
 client.login(token);
