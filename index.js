77a23cd2d9f6a76a490931a7d77f0df034d0fccb
MgrGracz <kamil.bandzwolek@gmail.com>
2024-07-01 15:22:07 +0200

    v004

--- index.js
+++ index.js
@@ -27,66 +27,90 @@ client.on('messageCreate', async (message) => {
     }
 
     if (message.attachments.size > 0) {
-        if (message.attachments.size > 0) {
-            message.attachments.forEach(async (attachment) => {
-              if (attachment.name.endsWith('.csv')) {
-                const response = await axios.get(attachment.url, {
-                  responseType: 'stream'
-                });
-        
-                const filePath = './downloaded.csv';
-                const writer = fs.createWriteStream(filePath);
-        
-                response.data.pipe(writer);
-        
-                writer.on('finish', () => {
-                  console.log('Plik pobrany pomyślnie.');
-                  processCSV(filePath);
-                  message.channel.send('zapisano plik!');
-                });
-        
-                writer.on('error', (err) => {
-                  console.error('Błąd podczas zapisywania pliku:', err);
-                });
-              }
-            });
-          }
-        }
-    });
-
-function processCSV(filePath) {
-    const products = {};
-  
-    fs.createReadStream(filePath)
-      .pipe(csv({ separator: '\t' })) // Użycie tabulatora jako separatora
-      .on('data', (row) => {
-        const player = row.Gracz;
-        const item = row.Przedmiot;
-        const quantity = parseInt(row.Ilość, 10);
-  
-        if (!products[player]) {
-          products[player] = {};
-        }
-  
-        if (!products[player][item]) {
-          products[player][item] = 0;
-        }
-  
-        products[player][item] += quantity;
-    })
-    .on('end', () => {
-      const jsonFilePath = './grouped_products.json';
-      fs.writeFile(jsonFilePath, JSON.stringify(products, null, 2), (err) => {
-        if (err) {
-          console.error('Błąd podczas zapisywania pliku JSON:', err);
-        } else {
-          console.log(`Dane zostały zapisane do pliku ${jsonFilePath}`);
+        message.attachments.forEach(async (attachment) => {
+            if (attachment.name.endsWith('.csv')) {
+              const response = await axios.get(attachment.url, {
+                responseType: 'stream'
+              });
+      
+              const filePath = './downloaded.csv';
+              const writer = fs.createWriteStream(filePath);
+      
+              response.data.pipe(writer);
+      
+              writer.on('finish', () => {
+                console.log('Plik pobrany pomyślnie.');
+                processCSV(filePath, message);
+              });
+      
+              writer.on('error', (err) => {
+                console.error('Błąd podczas zapisywania pliku:', err);
+              });
+            }
+          });
         }
       });
-    })
-    .on('error', (error) => {
-      console.error(error);
-    });
-}
+
+      function processCSV(filePath, message) {
+        const products = {};
+      
+        fs.createReadStream(filePath)
+          .pipe(csv({ separator: '\t' })) // Użycie tabulatora jako separatora
+          .on('data', (row) => {
+            const player = row.Gracz;
+            const item = row.Przedmiot;
+            const quantity = parseInt(row.Ilość, 10);
+      
+            if (!products[player]) {
+              products[player] = {};
+            }
+      
+            if (!products[player][item]) {
+              products[player][item] = 0;
+            }
+      
+            products[player][item] += quantity;
+          })
+          .on('end', () => {
+            const formattedData = {};
+            
+            for (const player in products) {
+              formattedData[player] = [];
+      
+              for (const item in products[player]) {
+                if (products[player][item] !== 0) {
+                  formattedData[player].push({ Przedmiot: item, Ilość: products[player][item] });
+                }
+              }
+            }
+      
+            const jsonFilePath = './grouped_products.json';
+            fs.writeFile(jsonFilePath, JSON.stringify(formattedData, null, 2), (err) => {
+              if (err) {
+                console.error('Błąd podczas zapisywania pliku JSON:', err);
+              } else {
+                console.log(`Dane zostały zapisane do pliku ${jsonFilePath}`);
+                
+                // Generowanie stringa do wysłania
+                let resultString = 'Lista osób i produktów:\n';
+                for (const player in formattedData) {
+                  resultString += `Gracz: ${player}\n`;
+                  formattedData[player].forEach(item => {
+                    resultString += `  Przedmiot: ${item.Przedmiot}, Ilość: ${item.Ilość}\n`;
+                  });
+                }
+                
+                // Wysłanie wiadomości na Discord
+
+                message.channel.send(resultString)
+                  .then(() => console.log('Wiadomość wysłana.'))
+                  .catch(console.error);
+              }
+            });
+          })
+          .on('error', (error) => {
+            console.error(error);
+          });
+      }
 
 client.login(token);
